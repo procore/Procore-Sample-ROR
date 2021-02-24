8e49400d772381ceab924434c03f62c996dc3045
Nate Baer <njbbaer@gmail.com>
2021-02-23 23:24:34 -0800

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
-# This code is intended to be used **for training purposes only**.
+# This code is intended to be used **for training purposes only**.# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-
-  get 'login/index'
-
-  get 'users/me'
-
-  get 'users/home'
-
-  post 'login/refresh'
-
-  get 'login/callback'
-
-  get 'auth/procore/callback', to: 'login#callback'
-
-  get 'login/revoke'
-
   root 'login#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :login do
+    get 'index'
+    post 'refresh'
+    get 'callback'
+    get 'revoke'
+  end
+
+  namespace :users do
+    get 'me'
+    get 'home'
+  end
+
+  get 'auth/procore/callback', to: 'login#callback'
 end
